1b0ad91c493d7784b736670a77e07a2fb1a257c6
Muvidha <90436781+Muvidha@users.noreply.github.com>
2022-10-14 23:12:30 +0700

    Add files via upload

---
+++ Praktikum 6/011_Praktikum06_1.php
@@ -0,0 +1,128 @@
+<!-- Muvidha Fatmawati Putri - 21091397011 -->
+
+<?php 
+
+abstract class Vehicle {
+    private $load = 0;
+    protected $maxLoad = 0, $name;
+
+    protected function __construct($maxLoad, $name) {
+        $this->$maxLoad = $maxLoad;
+        $this->$name = $name;
+    }
+
+    public function getLoad() {
+        return $this->load;
+    }
+
+    public function getMaxLoad() {
+        echo 'Maksimal muatan ' . $this->name . ' ' ;
+        return $this->maxLoad;
+    }
+
+    public function addBox($weight) {
+        if ($this->load >= $this->maxLoad) {
+            echo "$this->name menambah muatan sebesar $weight <br>";
+            echo 'Muatan telah penuh tidak bisa menambah lagi';
+        }else {
+            $this->load += $weight;
+            echo "$this->name menambah muatan sebesar $weight";
+        }
+    }
+
+    abstract public function calcFuelNeeds();
+
+    protected function calcFuelEfficiency() {
+        $range = 1000000000;
+        $range /= $this->load;
+        return $range;
+    }
+
+    protected function calcTripDistance() {
+        return 100000;
+    }
+}
+
+class Truck extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+    
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+        
+        return ceil($fuel /= $trip);
+    }
+}
+
+class RiverBarge extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+
+        return ceil($fuel /= $trip);
+    }
+}
+$truck = new Truck(20000, 'Truk');
+$riverBarge = new RiverBarge(35000, 'Perahu');
+
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        
+    <title>PBO - Praktikum 6</title>
+</head>
+
+<body>
+    <div class="container">
+        <br>
+        <h2 class="text-center">PBO - Praktikum 6</h2>
+        <div class="row">
+            <div class="col-5 mx-auto border p-3 mt-2">
+                <h4 class="text-center"><strong>Soal 1</strong></h4>
+                <br><br>
+                <b><?= $truck->getMaxLoad() . ' kg'; ?> <br></b>
+                <br>
+                <?= $truck->addBox(3000) . ' kg'; ?> <br>
+                <?= $truck->addBox(8000) . ' kg'; ?> <br>
+                <?= $truck->addBox(9000) . ' kg'; ?> <br>
+               
+                    <?php 
+                        echo "Jadi, Butuh Bahan Bakar sebanyak " . $truck->calcFuelNeeds() . ' Liter'. '<br>';
+                    ?>
+                <br>
+                ------------------------------------------------
+                <br>
+                <br>
+                <b><?= $riverBarge->getMaxLoad() . ' kg'; ?> <br></b>
+                <br>
+                <?= $riverBarge->addBox(12000) . ' kg'; ?> <br>
+                <?= $riverBarge->addBox(10000) . ' kg'; ?> <br>
+                <?= $riverBarge->addBox(7000) . ' kg'; ?> <br>
+
+                    <?php 
+                        echo "Jadi, Butuh Bahan Bakar sebanyak " . $riverBarge->calcFuelNeeds() . ' Liter';
+                    ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
